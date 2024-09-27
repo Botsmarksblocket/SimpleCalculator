390f0d3a73c680a271be66b661799c5525992053
Edvard <edde113@hotmail.com>
2024-09-27 13:34:28 +0200

    Cleaning up

--- SimpleCalculator/Program.cs
+++ SimpleCalculator/Program.cs
@@ -12,13 +12,8 @@ namespace SimpleCalculator
         {
             while (true)
 
-            Console.WriteLine("Simple calculator");
-            Console.WriteLine("Enter operator (+, -, *, /): ");
-            char operation = Console.ReadKey().KeyChar;
-            Console.WriteLine("\nEnter the first number: ");
-            double num1 = Convert.ToDouble(Console.ReadLine());
-            Console.WriteLine("Enter the second number: ");
-            double num2 = Convert.ToDouble(Console.ReadLine());
+                Console.WriteLine("Simple calculator");
+                Console.WriteLine("-----------------");
 
             double result = 0;
             switch(operation)
@@ -38,8 +33,8 @@ namespace SimpleCalculator
                 default:
                     Console.WriteLine("Invalid operation.");
                     return;
+
             }
-            Console.WriteLine($"Result: {result}");
             Console.ReadKey();
         }
     }
