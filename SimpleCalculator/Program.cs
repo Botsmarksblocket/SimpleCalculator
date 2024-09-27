2218442e5f17ab0b5ea82dffc7350a339b63d014
Edvard <edde113@hotmail.com>
2024-09-27 10:03:44 +0200

    Add project files.

---
+++ SimpleCalculator/Program.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SimpleCalculator
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+
+            Console.WriteLine("Simple calculator");
+            Console.WriteLine("Enter operator (+, -, *, /): ");
+            char operation = Console.ReadKey().KeyChar;
+            Console.WriteLine("\nEnter the first number: ");
+            double num1 = Convert.ToDouble(Console.ReadLine());
+            Console.WriteLine("Enter the second number: ");
+            double num2 = Convert.ToDouble(Console.ReadLine());
+
+            double result = 0;
+            switch(operation)
+            {
+                case '+':
+                    result = num1 + num2;
+                    break;
+                case '-':
+                    result = num1 - num2;
+                    break;
+                case '*':
+                    result = num1 * num2;
+                    break;
+                case '/':
+                    result = num1 / num2;
+                    break;
+                default:
+                    Console.WriteLine("Invalid operation.");
+                    return;
+            }
+            Console.WriteLine($"Result: {result}");
+            Console.ReadKey();
+        }
+    }
+}
