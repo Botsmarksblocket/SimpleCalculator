deb02b74778bd2433802929667c5b28f3d09c914
Edvard <edde113@hotmail.com>
2024-09-27 13:37:05 +0200

    The user input is changed from char to string

--- SimpleCalculator/Program.cs
+++ SimpleCalculator/Program.cs
@@ -22,8 +22,35 @@ namespace SimpleCalculator
                 if (userInput.ToLower() == "exit")//Breaks the while-loop which allows user to exit the program
                     break;
 
+                Console.WriteLine("\nEnter the first number: ");
+                double num1 = Convert.ToDouble(Console.ReadLine());
+                Console.WriteLine("Enter the second number: ");
+                double num2 = Convert.ToDouble(Console.ReadLine());
+
+                double result = 0;
+                switch (userInput)
+                {
+                    case "+":
+                        result = num1 + num2;
+                        break;
+                    case "-":
+                        result = num1 - num2;
+                        break;
+                    case "*":
+                        result = num1 * num2;
+                        break;
+                    case "/":
+                        result = num1 / num2;
+                        break;
+                    default:
+                        Console.WriteLine("Invalid operation.");
+                        return;
+                }
+                Console.WriteLine($"Result: {result}");
+                inputMethods.ContinuePrompt();
+                Console.Clear();
             }
-            Console.ReadKey();
+            Console.WriteLine("You have exited the program!");
         }
     }
 }
