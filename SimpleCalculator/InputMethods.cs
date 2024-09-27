0ada794882d520a1e9acb6a531c647b7e1caa20e
Edvard <edde113@hotmail.com>
2024-09-27 13:31:47 +0200

    New class with InputMethods

---
+++ SimpleCalculator/InputMethods.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SimpleCalculator
+{
+    public class InputMethods
+    {
+        public void ContinuePrompt()
+        {
+            Console.WriteLine("Press any key to continue...");
+            Console.ReadKey();
+        }
+        public string ValidOperator()
+        {
+            while (true)
+            {
+                Console.WriteLine("Enter operator (+, -, *, /) or type 'exit' to quit: ");
+                string checkOperator = Console.ReadLine();
+
+                if (checkOperator.Length != 1 || "+-*/".IndexOf(checkOperator) == -1)
+                {
+                    Console.Clear();
+                    Console.WriteLine("Invalid operator, please try again.");
+                }
+                else
+                {
+                    return checkOperator;
+                }
+            }
+        }
+    }
+}
